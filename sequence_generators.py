c453f83cc281156bee7c8698da53462a928fdd79
henrique-britoleao <hbl451@gmail.com>
2021-02-27 11:58:37 +0100

    Final commit

--- sequence_generators.py
+++ sequence_generators.py
@@ -1,8 +1,9 @@
 ################################################################################
-# Project Euler: Problem 167
+# Simple Sequence Generator
 ################################################################################
-import time
+
 def get_next_n(last_n, sums_dict):
+    '''Returns smallest key in dictionary with value = 1'''
     delta = 1
     while True:
         try:
@@ -15,7 +16,8 @@ def get_next_n(last_n, sums_dict):
         except:
             delta += 1 
 
-def problem_167(v, n_iter):
+def ulam_generator(v, n_iter):
+    '''Generates first n_iter+2 items of U(2,v) sequence'''
     sequence = [2, v]
     sums = {}
     count = 0 
@@ -31,10 +33,11 @@ def problem_167(v, n_iter):
     return sequence
 
 ################################################################################
-# Introduce knowledge of even numbers
+# Bonus: optimal sequence generator
 ################################################################################
 
-def problem_167_v2(v, n_iter):
+def ulam_generator_optim(v, n_iter):
+    '''Generates first n_iter+2 items of U(2,v) sequence'''
     sequence = [2, v]
     sums = {}
     count = 0 
@@ -47,6 +50,7 @@ def problem_167_v2(v, n_iter):
                                                 # can be represented as a sum of 
                                                 # previous items
         elif count == (v+7)/2:
+            # erase all even terms from dictionary
             sums = {key:value for key, value in sums.items() if key%2 != 0}
             temp_1 = sequence[-1] + 2
             temp_2 = sequence[-1] + 2*v+2
@@ -61,43 +65,3 @@ def problem_167_v2(v, n_iter):
         next_n, sums = get_next_n(sequence[-1], sums)
         sequence.append(next_n)
     return sequence
-
-# start_time = time.time()
-# problem_167_v2(5, 1e7)
-# print(f'Run time: {time.time() - start_time}')
-
-################################################################################
-# Optimized version
-################################################################################
-
-# def get_next_n_opt(n_hat, sequence, v):
-#     test_1 = 2*(n_hat-1) + 1 in sequence
-#     test_2 = 2*(n_hat-v-1) + 1 in sequence
-#     if test_1 ^ test_2:
-#         return 2*n_hat + 1
-#     else:
-#         pass
-             
-         
-# def problem_167_opt(v, n_items):
-#     sequence = [2, v]
-#     sums = {}
-#     largest_even = 2*v + 2
-#     largest_even_i = (v+7)/2
-#     while len(sequence) < largest_even_i:
-#         for item in sequence[:-1]:
-#             temp = sequence[-1] + item
-#             sums[temp] = sums.get(temp, 0) + 1 
-#         next_n, sums = get_next_n(sequence[-1], sums)
-#         sequence.append(next_n)
-
-#     del sums
-#     sequence = set(sequence) # optimize search times
-#     n_hat = int(largest_even/2) # int avoids float outputs
-    
-#     while len(sequence) < 32 + 1 + largest_even_i:
-#         sequence.add(get_next_n_opt(n_hat, sequence, v))
-#         n_hat += 1
-
-#     return sequence
-
--- solution.py
+++ solution.py
@@ -1,44 +1,68 @@
-import problem
+from sequence_generators import ulam_generator
 import sys
 
 class Ulam:
     '''
-    # TODO
+    Class that solves Problem 167 from Project Euler
+
+    Attributes
+    ----------
+    n: int
+        Number with wich to build the Ulam sequnce U(2, 2*n + 1)
+    __periods: list
+        Periodicity of the sequence depending on n
+    __fun_diff: list
+        Fundamental difference of items in sequence depending on n
+    __largest_even: int
+        Largest even number in the sequence
+    __largest_even_i: int
+        Index of the largest even number
+
+    Parameters
+    ----------
+    n: int
+        Number with wich to build the Ulam sequnce U(2, 2*n + 1)
     '''
     def __init__(self, n):
         self.n = n 
         self.__periods = [32, 26, 444, 1628, 5906, 80, 126960, 380882, 2097152]
         self.__fund_diff = [126, 126, 1778, 6510, 23622, 510, 507842, 1523526,
                             8388606]
-        self.largest_even = 4*n + 4
-        self.largest_even_i = int((2*n + 8)/2)
+        self.__largest_even = 4*n + 4
+        self.__largest_even_i = int((2*n + 8)/2)
     
     def get_item(self, k):
-        '''
-        # TODO
-        '''
-        self.get_first_elements()
-        if k < self.largest_even:
-            print(f'The {k}th element of the sequence U(2,{2*self.n + 1}) is: {self.first_elements[int(k-1)]}')
+        '''Returns the k-th item from the Ulam sequence.'''
+        self._build_first_elements()
+        
+        if k < self.__largest_even: # no need to build period
+            print(f"""The {k}th element of the sequence U(2,{2*self.n + 1}) is:\
+                      {self.first_elements[int(k-1)]}""")
+            
             return self.first_elements[int(k-1)]
+        
         else:
-            self.get_first_period()
-            n_periods, last_k = divmod(int(k) - self.largest_even_i, self.__periods[self.n - 2])
-            last_period = list(self.n_period_generator(n_periods))
+            self._build_first_period() 
+            n_periods, last_k = divmod(
+                int(k) - self.__largest_even_i, self.__periods[self.n - 2]
+            ) # subtract largest_even_i to get number of periods needed
+            last_period = list(self._n_period_generator(n_periods)) 
 
-            print(f'The {k}th element of the sequence U(2,{2*self.n + 1}) is: {last_period[last_k - 1]}')
-        return last_period[last_k - 1]
+            print(f"""The {k}th element of the sequence U(2,{2*self.n + 1}) is: 
+                      {last_period[last_k - 1]}""")
+            
+            return last_period[last_k - 1]
 
-    def get_first_elements(self):
-        
-        self.first_elements = problem.problem_167(2*self.n + 1, self.largest_even_i - 2)
+    def _build_first_elements(self):
+        '''Builds first elements auxiliary sequence'''
+        self.first_elements = ulam_generator(2*self.n+1, self.__largest_even_i-2)
     
-    def get_first_period(self):
+    def _build_first_period(self):
+        '''Builds period auxiliary sequence'''
+        n_hat = int(self.__largest_even/2) # int avoids float outputs
+        self.period = set(self.first_elements) # improve search time
         
-        n_hat = int(self.largest_even/2) # int avoids float outputs
-        self.period = set(self.first_elements)
-        
-        while len(self.period) < self.__periods[self.n - 2] + self.largest_even_i: # minimum n = 2
+        while len(self.period) < self.__periods[self.n - 2] + self.__largest_even_i: # minimum n = 2
             
             test_1 = 2*(n_hat-1) + 1 in self.period
             test_2 = 2*(n_hat-2*self.n-2) + 1 in self.period
@@ -49,16 +73,18 @@ class Ulam:
                 pass
 
             n_hat += 1
+
         self.period = self.period - set(self.first_elements)
         self.period = sorted(list(self.period))
 
-    def n_period_generator(self, n_period):
+    def _n_period_generator(self, n_period):
+        '''Generates numbers in the n-th occurence of the period'''
         for number in self.period:
             yield number + n_period*self.__fund_diff[self.n-2]
     
 
 def main():
-    total_sum = sum([Ulam(n).get_item(5) for n in range(2, 10+1)])
+    total_sum = sum([Ulam(n).get_item(1e11) for n in range(2, 10+1)])
     print(f'The result of the sum is: {total_sum}')
 
 if __name__ == '__main__':
